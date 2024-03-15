3f320707bafd85cadfa5a3ec52655869f2decdb8
Douglas Bello <dgbello.dev@hotmail.com>
2024-03-15 13:44:35 -0300

    [frontend] - criando rota de cursos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggle } from "@angular/material/slide-toggle";
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatIcon } from "@angular/material/icon";
+import { MatIconButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSlideToggle, MatToolbar, MatIcon, MatIconButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
